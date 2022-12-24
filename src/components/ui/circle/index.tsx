5024055dbcd0b104023ab922b6b32c388d567b30
Nikolay Smolov <n.smolov@yandex.ru>
2022-12-24 21:31:18 +0300

    adds unit tests for circle component

--- src/components/ui/circle/index.tsx
+++ src/components/ui/circle/index.tsx
@@ -8,7 +8,6 @@ interface CircleProps {
   head?: string | React.ReactElement | null;
   index?: number;
   tail?: string | React.ReactElement | null;
-  tailType?: 'string' | 'element';
   extraClass?: string;
   isSmall?: boolean;
 }
@@ -23,14 +22,16 @@ export const Circle: React.FC<CircleProps> = ({
   isSmall,
 }) => {
   return (
-    <div className={`${styles.content} ${extraClass}`}>
+    <div className={`${styles.content} ${extraClass}`} data-testid="circle">
       <div
         className={`text text_type_input text_color_input mb-4 ${styles.absolute} ${styles.head} ${
           styles[typeof head === 'string' ? 'string' : 'element']
         }`}>
         {head}
       </div>
-      <div className={`${styles.circle}  ${isSmall ? styles.small : ''} ${styles[state]}`}>
+      <div
+        data-testid="circle-main"
+        className={`${styles.circle}  ${isSmall ? styles.small : ''} ${styles[state]}`}>
         <p className={`text text_type_circle text_color_input ${styles.letter}`}>{letter}</p>
       </div>
       <p
