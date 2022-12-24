aeeb1a91ecd5b14b1e89c88c1d253cef8bfae595
Nikolay Smolov <n.smolov@yandex.ru>
2022-12-24 19:42:17 +0300

    adds unit tests for button component

---
+++ src/components/ui/button/button.test.tsx
@@ -0,0 +1,42 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Button } from '.';
+
+describe('Button component test', () => {
+  it('should render with text', () => {
+    const { getByRole } = render(<Button text="clickMe" />);
+    const button = getByRole('button');
+    expect(button).toMatchSnapshot();
+  });
+
+  it('should render without text', () => {
+    const { getByRole } = render(<Button />);
+    const button = getByRole('button');
+    expect(button).toMatchSnapshot();
+  });
+
+  it('should render disabled state', () => {
+    const { getByRole } = render(<Button disabled />);
+    const button = getByRole('button');
+    expect(button).toMatchSnapshot();
+  });
+
+  it('should render loading state', () => {
+    const { getByRole } = render(<Button isLoader />);
+    const button = getByRole('button');
+    expect(button).toMatchSnapshot();
+  });
+
+  it('should click', () => {
+    const click = jest.fn();
+    const { getByRole } = render(<Button onClick={click} />);
+
+    const button = getByRole('button');
+
+    expect(click).toBeCalledTimes(0);
+
+    userEvent.click(button);
+
+    expect(click).toHaveBeenCalledTimes(1);
+  });
+});
